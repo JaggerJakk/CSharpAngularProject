76f283fb552eb7ec0d572aab63b4c3ad37e85fc6
Jagger Jakk <ivon.jakk@gmail.com>
2022-07-08 18:50:23 -0400

    Initial release

---
+++ DatingApp/DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController] // I think it ommits the /api/
+    [Route("[controller]")] // controller = Values
+    // Works on https://localhost:7234/values
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(value);
+        }
+    }
+}
---
+++ DatingApp/DatingApp.API/Models/Value.cs
@@ -0,0 +1,8 @@
+﻿namespace DatingApp.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; } // EF always use this as the primary key
+        public string Name { get; set; }
+    }
+}
